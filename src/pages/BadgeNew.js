be1c3bd7a60b33a3617aef69191189e05d32e961
Nayllen Soleika Rojas Martinez <nayllen.rojas@gmail.com>
2021-04-05 23:03:53 -0500

    chore(componentes/): crea página

---
+++ src/pages/BadgeNew.js
@@ -0,0 +1,33 @@
+import React from "react";
+import NavBar from "../components/Navbar";
+import header from "../images/badge-header.svg";
+import Badge from "../components/Badge";
+import avatar from "../images/avatar.png";
+
+class BadgeNew extends React.Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <div className="BadgeNew__hero">
+          <img src={header} alt="logo" className="img-fluid" />
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <Badge
+                firstName="Nay"
+                lastName="Rojas"
+                jobtTitle="Frontend Engineer"
+                twitter="@nayrojas_"
+                avatar={avatar}
+              ></Badge>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default BadgeNew;
